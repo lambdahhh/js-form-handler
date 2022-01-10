e646b320b7806cffc058d22c50666546f85df155
pvolkov <p.volkov@ndv.ru>
2022-01-11 01:31:18 +0300

    failure dev server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+console.log(path.join(__dirname, 'dist'));
 module.exports = {
     entry: './src/js/main.js',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        // static: {
+        //     directory: path.join(__dirname, 'dist'),
+        // },
+        compress: true,
+        port: 3000,
     },
     plugins: [
         new CopyWebpackPlugin({
